928a31e29fa9a1b5338b00d05fd875bc64cc1d21
hbin1911 <rajas.mulik@gmail.com>
2023-08-20 19:03:53 +0530

    eshop

---
+++ src/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './NavBar.css';
+
+export default function NavBar() {
+  return (
+    <div >
+        <header className='container'>
+      
+        <div className='logo'>
+        <h2>upGrad</h2>
+        </div>
+        <div className='btn'>
+            <button className='btn-left'>Login</button>
+            <button className='btn-right'>Signup</button>
+        </div>
+        </header>
+      
+    </div>
+  )
+}
